d5db777febe1bc8853fd137a548e8a46085a4420
y <yzr@yserver.top>
2024-01-07 00:40:07 +0000

    From alexbelgium/hassio-addons

---
+++ calibre/rootfs/etc/cont-init.d/01-migrate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/with-contenv bashio
+# shellcheck shell=bash
+set -e
+
+####################
+# Migrate database #
+####################
+
+if [ -d /homeassistant/addons_config/calibre ]; then
+    echo "Moving database to new location /config"
+    cp -rf /homeassistant/addons_config/calibre/* /config/
+    rm -r /homeassistant/addons_config/calibre
+fi
+
+# Legacy path
+if [ -d /config/addons_config/calibre ]; then rm -r /config/addons_config/calibre; fi
+mkdir -p /config/addons_config/calibre
+ln -s "/config/Calibre Library" "/config/addons_config/calibre/"
+chmod -R 777 /config/addons_config
